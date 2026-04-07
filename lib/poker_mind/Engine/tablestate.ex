e3df761efa6f50c1f759e77efaac078b1959e55e
Stine Gammelager <stinegammelager00@gmail.com>
2026-04-07 13:14:00 +0200

    feat: implemented set_blinds (#30)
    * feat: implemented set_blinds
    
    * fix: added big_blind to struct
    
    * fix: mix format
    
    * fix: updated set_blinds, advance_player and tests
    
    formatting

--- lib/poker_mind/Engine/tablestate.ex
+++ lib/poker_mind/Engine/tablestate.ex
@@ -13,6 +13,8 @@ defmodule PokerMind.Engine.TableState do
     :deck,
     # cards on the table
     :community_cards,
+    # big blind
+    :small_blind,
     # whose turn
     :current_player,
     # bet to match
@@ -26,9 +28,9 @@ defmodule PokerMind.Engine.TableState do
   def init(state, init_players) when is_list(init_players) do
     state
     |> initialize_players(init_players)
-    # |> set_blinds()
     |> new_deck()
     |> deal_cards()
+    |> set_blinds()
   end
 
   defp initialize_players(table_state, []) do
@@ -42,6 +44,29 @@ defmodule PokerMind.Engine.TableState do
   defp add_player(%{players: players} = tablestate, %{stack_size: _} = new_player)
        when is_list(players) do
     Map.put(tablestate, :players, [new_player | players])
+  end
+
+  defp set_blinds(table_state) do
+    small_blind = Enum.random(table_state.players)
+
+    table_state
+    |> Map.put(:small_blind, small_blind)
+    |> advance_player(:current_player, small_blind)
+    |> advance_player()
+  end
+
+  def advance_player(table_state, key \\ :current_player, player \\ nil) do
+    from_player =
+      case player do
+        nil -> table_state.current_player
+        player -> player
+      end
+
+    index = Enum.find_index(table_state.players, fn p -> p == from_player end)
+    next_player = Enum.at(table_state.players, rem(index + 1, length(table_state.players)))
+
+    table_state
+    |> Map.put(key, next_player)
   end
 
   defp new_deck(table_state) do
--- test/poker_mind/Engine/tablestate_test.exs
+++ test/poker_mind/Engine/tablestate_test.exs
@@ -34,4 +34,54 @@ defmodule PokerMind.Engine.TableStateTest do
       assert !Enum.member?(state.deck, card)
     end
   end
+
+  test "set_blinds/1 - more than 2 players, a player is chosen as small blind and another as current player",
+       %{
+         state: state
+       } do
+    assert Enum.member?(state.players, state.small_blind)
+    assert Enum.member?(state.players, state.current_player)
+    assert state.small_blind != state.current_player
+  end
+
+  test "set_blinds/1 - exactly 2 players, a player is chosen as both small blind and current player" do
+    players =
+      [
+        %{name: "stine", stack_size: 100_000, cards: []},
+        %{name: "rolf", stack_size: 100_000, cards: []}
+      ]
+
+    state = TableState.init(TableState.new(), players)
+
+    assert Enum.member?(state.players, state.small_blind)
+    assert Enum.member?(state.players, state.current_player)
+    assert state.small_blind == state.current_player
+  end
+
+  test "advance_player/2 - current player becomes next player in the list", %{
+    state: state
+  } do
+    player_idx_0 = Enum.at(state.players, 0)
+
+    new_state =
+      state
+      |> Map.put(:current_player, player_idx_0)
+      |> TableState.advance_player()
+
+    assert Enum.find_index(new_state.players, fn p -> p == new_state.current_player end) == 1
+  end
+
+  test "advance_player/2 - current player becomes first player in the list if the player before was last in the list",
+       %{
+         state: state
+       } do
+    player_idx_last = Enum.at(state.players, length(state.players) - 1)
+
+    new_state =
+      state
+      |> Map.put(:current_player, player_idx_last)
+      |> TableState.advance_player()
+
+    assert Enum.find_index(new_state.players, fn p -> p == new_state.current_player end) == 0
+  end
 end
