1c84f8555235665eecb2bc086b2ca4a986bb863d
Stine Gammelager <stinegammelager00@gmail.com>
2026-04-07 13:28:48 +0200

    fix: pattern match on state

--- lib/poker_mind/Engine/tablestate.ex
+++ lib/poker_mind/Engine/tablestate.ex
@@ -26,7 +26,7 @@ defmodule PokerMind.Engine.TableState do
     %__MODULE__{id: "123", phase: :pre_flop, players: [], pot: 0, deck: [], community_cards: []}
   end
 
-  def init(state, init_players) when is_list(init_players) do
+  def init(%__MODULE__{} = state, init_players) when is_list(init_players) do
     state
     |> initialize_players(init_players)
     |> new_deck()
@@ -34,20 +34,20 @@ defmodule PokerMind.Engine.TableState do
     |> set_blinds()
   end
 
-  defp initialize_players(state, []) do
+  defp initialize_players(%__MODULE__{} = state, []) do
     state
   end
 
-  defp initialize_players(state, [hd | rest]) do
+  defp initialize_players(%__MODULE__{} = state, [hd | rest]) do
     initialize_players(add_player(state, hd), rest)
   end
 
-  defp add_player(state, new_player)
+  defp add_player(%__MODULE__{} = state, new_player)
        when is_list(state.players) do
     Map.put(state, :players, [new_player | state.players])
   end
 
-  defp set_blinds(state) do
+  defp set_blinds(%__MODULE__{} = state) do
     small_blind = Enum.random(state.players)
 
     state
@@ -56,7 +56,7 @@ defmodule PokerMind.Engine.TableState do
     |> advance_player()
   end
 
-  def advance_player(state, key \\ :current_player, player \\ nil) do
+  def advance_player(%__MODULE__{} = state, key \\ :current_player, player \\ nil) do
     from_player =
       case player do
         nil -> state.current_player
@@ -70,7 +70,7 @@ defmodule PokerMind.Engine.TableState do
     |> Map.put(key, next_player)
   end
 
-  defp new_deck(state) do
+  defp new_deck(%__MODULE__{} = state) do
     # TODO: Extract as compile-time constants
     suits = [:hearts, :diamonds, :clubs, :spades]
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
@@ -85,7 +85,7 @@ defmodule PokerMind.Engine.TableState do
     |> Map.put(:deck, deck)
   end
 
-  def deal_cards(state) do
+  def deal_cards(%__MODULE__{} = state) do
     {updated_players, remaining_deck} =
       Enum.map_reduce(state.players, state.deck, fn player, acc ->
         {drawn, remaining} = Enum.split(acc, 2)
