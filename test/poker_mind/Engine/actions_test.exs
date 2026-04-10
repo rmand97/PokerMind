f62d9f257f364ccec1c4d11e663b9fa28d633aef
Rolf Andersen <rolf.m.andersen@it-minds.dk>
2026-04-10 13:30:51 +0200

    feat: add check action

--- test/poker_mind/Engine/actions_test.exs
+++ test/poker_mind/Engine/actions_test.exs
@@ -13,9 +13,9 @@ defmodule PokerMind.Engine.ActionsTest do
 
   test "fold action", %{state: init_state} do
     # get current player
-    assert init_state.current_player != nil
-    player_who_is_folding = init_state.current_player.id
-    # assert init_state.small_blind +2  == init_state.current_player
+    assert init_state.current_player_id != nil
+    player_who_is_folding = init_state.current_player_id
+    # assert init_state.small_blind +2  == init_state.current_player_id
 
     # Check that current player is in players list and "active_in_hand"
     assert Enum.any?(
@@ -26,7 +26,7 @@ defmodule PokerMind.Engine.ActionsTest do
            )
 
     # apply fold action to current player
-    new_state = Actions.apply_action(init_state, :fold, init_state.current_player.id)
+    new_state = Actions.apply_action(init_state, :fold, init_state.current_player_id)
 
     # Have the player succesfully folded?
     folded_player =
@@ -40,7 +40,7 @@ defmodule PokerMind.Engine.ActionsTest do
     assert unchanged_players == original_others
 
     # Turn has advanced — new current player is not the one who folded
-    assert new_state.current_player != player_who_is_folding
+    assert new_state.current_player_id != player_who_is_folding
 
     # Pot size is unchanged — folding doesn't affect the pot
     assert new_state.pot == init_state.pot
@@ -57,16 +57,26 @@ defmodule PokerMind.Engine.ActionsTest do
   #   # pre player still "active"
   # end
 
-  # test "check action", %{state: init_state} do
-  #   # get current player
-  #   # apply check action to current player
+  test "check action", %{state: init_state} do
+    starting_player = init_state.current_player_id
 
-  #   # Pot size the same
-  #   # Player stack the same
+    dbg(starting_player)
 
-  #   # new current player
-  #   # pre player still "active"
-  # end
+    new_state = Actions.apply_action(init_state, :check, starting_player)
+
+    dbg(new_state.players)
+    dbg(new_state.current_player_id)
+    assert Enum.any?(new_state.players, & &1.id == starting_player and &1.has_acted)
+    assert starting_player != new_state.current_player_id
+    # get current player
+    # apply check action to current player
+
+    # Pot size the same
+    # Player stack the same
+
+    # new current player
+    # pre player still "active"
+  end
 
   # test "raise action", %{state: init_state} do
   #   # get current player
