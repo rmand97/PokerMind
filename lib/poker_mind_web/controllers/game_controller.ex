461d7771928a0abdba7da92c619a4f3a9173f4fb
Stine Gammelager <stinegammelager00@gmail.com>
2026-04-30 13:36:35 +0200

    add functionality for close_suite endpoint

--- lib/poker_mind_web/controllers/game_controller.ex
+++ lib/poker_mind_web/controllers/game_controller.ex
@@ -15,6 +15,8 @@ defmodule PokerMindWeb.GameController do
   alias PokerMindWeb.Schemas.StartSuiteRequest
   alias PokerMindWeb.Schemas.StartSuiteResponse
   alias PokerMindWeb.Schemas.SuitesResponse
+  alias PokerMindWeb.Schemas.CloseSuiteRequest
+  alias PokerMindWeb.Schemas.CloseSuiteResponse
 
   operation(:suites,
     summary: "List all match suites",
@@ -57,6 +59,32 @@ defmodule PokerMindWeb.GameController do
     conn
     |> put_status(:bad_request)
     |> json(%{error: "players are required"})
+  end
+
+  operation(:close_suite,
+    summary: "Close a game suite",
+    request_body: {"Close suite params", "application/json", CloseSuiteRequest},
+    responses: [
+      ok: {"Closed suite", "application/json", CloseSuiteResponse},
+      not_found: {"Not found", "application/json", NotFound},
+      bad_request: {"Bad request", "application/json", BadRequest}
+    ]
+  )
+
+  def close_suite(conn, %{"id" => suite_id}) do
+    case MatchSupervisor.close_match_suite(suite_id) do
+      :ok ->
+        json(conn, %{})
+
+      {:error, :suite_not_found} ->
+        conn |> put_status(:not_found) |> json(%{error: "suite_id not found"})
+    end
+  end
+
+  def close_suite(conn, _params) do
+    conn
+    |> put_status(:bad_request)
+    |> json(%{error: "suite_id is required"})
   end
 
   operation(:next_games,
