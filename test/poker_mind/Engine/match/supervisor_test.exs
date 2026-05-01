3db59553b2ebc1da3ae53e54279aecf6625fbbb8
Rolf Andersen <rolf.m.andersen@it-minds.dk>
2026-05-01 13:30:00 +0200

    chore: add test for shutting down chidlren

--- test/poker_mind/Engine/match/supervisor_test.exs
+++ test/poker_mind/Engine/match/supervisor_test.exs
@@ -43,24 +43,25 @@ defmodule PokerMind.Engine.Match.SupervisorTest do
       assert suites[suite2_id] == players2
     end
 
-    test "" do
-      suite1_id = UUID.uuid4()
-      suite2_id = UUID.uuid4()
-      players1 = ["rolf", "stine"]
-      players2 = ["asbjørn"]
+    test "close_match_suite/1 properly closes suite and its children" do
+      suite_id = UUID.uuid4()
+      players = ["rolf", "stine"]
+      {:ok, pid, ^suite_id} = MatchSupport.start_match_suite!(suite_id, players)
+      assert %{^suite_id => ^players} = MatchSupervisor.all_match_suites()
 
-      {:ok, _, ^suite1_id} = MatchSupport.start_match_suite!(suite1_id, players1)
-      {:ok, _, ^suite2_id} = MatchSupport.start_match_suite!(suite2_id, players2)
+      children = DynamicSupervisor.which_children(pid)
+      assert length(children) == 11
 
-      on_exit(fn ->
-        MatchSupervisor.close_match_suite(suite1_id)
-        MatchSupervisor.close_match_suite(suite2_id)
+      game_pids = Enum.map(children, fn child -> elem(child, 1) end)
+
+      refs = Enum.map(game_pids, fn game_pid -> {game_pid, Process.monitor(game_pid)} end)
+
+      assert :ok = MatchSupervisor.close_match_suite(suite_id)
+      refute Process.alive?(pid)
+
+      Enum.each(refs, fn {game_pid, ref} ->
+        assert_receive {:DOWN, ^ref, :process, ^game_pid, _reason}, 5_000
       end)
-
-      suites = MatchSupervisor.all_match_suites()
-
-      assert suites[suite1_id] == players1
-      assert suites[suite2_id] == players2
     end
   end
 end
